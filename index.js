f09492621b6bbb826c69714c2eb7aaa49bf051e4
anahi <anahimayancela@gmail.com>
2020-10-12 23:00:12 -0400

    change background color on button click

---
+++ index.js
@@ -0,0 +1,16 @@
+window.onload = function() { //event hooking being used to prevent style == null
+    container();
+    function container() {
+        const button = document.querySelector('button')
+        const body = document.querySelector('body')
+        const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+
+        body.style.backgroundColor = 'violet'
+        button.addEventListener('click', changeBackground)
+
+        function changeBackground() {
+        const colorIndex= parseInt(Math.random()*colors.length)
+        body.style.backgroundColor = colors[colorIndex]
+        }
+    }
+}
