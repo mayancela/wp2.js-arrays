73bd9ec7caadfaf09a28e7034ef72199ef53f5c4
anahi <anahimayancela@gmail.com>
2020-10-13 00:45:42 -0400

    added buttton to generate random background color and/or a monochromatic background color

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 window.onload = function() { //event hooking being used to prevent style == null
     container();
     function container() {
-        const button = document.querySelector('button')
-        const body = document.querySelector('body')
-        const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+        const random_button = document.getElementById('random');
+        const monochrome_button = document.getElementById('monochromatic');
+        const body = document.querySelector('body');
+        const random_colors = ['#79E359', '#E69D68', '#4DBCD5', '#2C76D6', '#CC6012', '#AD0000', '#FFE4B7', '#8975FF'];
+        const monochrome_colors = ['#BBDFFA', '#97BAEC', '#445BC1', '#2C43B8', '#1727AE'];
+        var count = 0;
 
-        body.style.backgroundColor = 'violet'
-        button.addEventListener('click', changeBackground)
+        body.style.backgroundColor = '#eee';
+        random_button.addEventListener('click', chooseRandomColor);
+        monochrome_button.addEventListener('click', chooseMonochromaticColor);
 
-        function changeBackground() {
-        const colorIndex= parseInt(Math.random()*colors.length)
-        body.style.backgroundColor = colors[colorIndex]
+        function chooseRandomColor() {
+            const colorIndex= parseInt(Math.random()*random_colors.length);
+            body.style.backgroundColor = random_colors[colorIndex];
+            console.log("Array length: " + random_colors.length);
+            console.log("Index number chosen: " + colorIndex);
+        }
+
+        function chooseMonochromaticColor() {
+            var length =  monochrome_colors.length;
+            console.log("Array length: " + length);
+            console.log("Index is at: " + count);
+
+            if(count != length) {
+                body.style.backgroundColor = monochrome_colors[count];
+                count++;
+            }
+            else {
+                count = 0;
+                body.style.backgroundColor = monochrome_colors[count];
+                count++;
+            }
+
         }
     }
 }
