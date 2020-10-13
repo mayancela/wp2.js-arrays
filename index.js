d316df9e4e3f027b6a8c5345e8c4f5ed2a3bbe98
anahi <anahimayancela@gmail.com>
2020-10-13 00:55:38 -0400

    Added button to choose pink monochromatic background color

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ window.onload = function() { //event hooking being used to prevent style == null
     container();
     function container() {
         const random_button = document.getElementById('random');
-        const monochrome_button = document.getElementById('monochromatic');
+        const monochrome_button_b = document.getElementById('monochromatic-b');
+        const monochrome_button_p = document.getElementById('monochromatic-p');
         const body = document.querySelector('body');
         const random_colors = ['#79E359', '#E69D68', '#4DBCD5', '#2C76D6', '#CC6012', '#AD0000', '#FFE4B7', '#8975FF'];
-        const monochrome_colors = ['#BBDFFA', '#97BAEC', '#445BC1', '#2C43B8', '#1727AE'];
+        const monochrome_colors_b = ['#A0D2E7', '#81B1D5', '#3D60A7', '#26408B'];
+        const monochrome_colors_p = ['#FFB6D5', '#FF99CC', '#F47DBB', '#D94496'];
+
         var count = 0;
 
         body.style.backgroundColor = '#eee';
         random_button.addEventListener('click', chooseRandomColor);
-        monochrome_button.addEventListener('click', chooseMonochromaticColor);
+        monochrome_button_b.addEventListener('click', chooseBlueMonochromaticColor);
+        monochrome_button_p.addEventListener('click', choosePinkMonochromaticColor);
+
 
         function chooseRandomColor() {
             const colorIndex= parseInt(Math.random()*random_colors.length);
@@ -19,18 +24,34 @@ window.onload = function() { //event hooking being used to prevent style == null
             console.log("Index number chosen: " + colorIndex);
         }
 
-        function chooseMonochromaticColor() {
-            var length =  monochrome_colors.length;
+        function chooseBlueMonochromaticColor() {
+            var length =  monochrome_colors_b.length;
             console.log("Array length: " + length);
             console.log("Index is at: " + count);
 
             if(count != length) {
-                body.style.backgroundColor = monochrome_colors[count];
+                body.style.backgroundColor = monochrome_colors_b[count];
                 count++;
             }
             else {
                 count = 0;
-                body.style.backgroundColor = monochrome_colors[count];
+                body.style.backgroundColor = monochrome_colors_b[count];
+                count++;
+            }
+        }
+
+        function choosePinkMonochromaticColor() {
+            var length =  monochrome_colors_p.length;
+            console.log("Array length: " + length);
+            console.log("Index is at: " + count);
+
+            if(count != length) {
+                body.style.backgroundColor = monochrome_colors_p[count];
+                count++;
+            }
+            else {
+                count = 0;
+                body.style.backgroundColor = monochrome_colors_p[count];
                 count++;
             }
 
